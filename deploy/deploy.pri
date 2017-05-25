377229f6d6494a56b622f005ff0521ea35eea6aa
Skycoder42 <Skycoder42@users.noreply.github.com>
2017-05-25 18:02:54 +0200

    python deploy script

--- deploy/deploy.pri
+++ deploy/deploy.pri
@@ -1,25 +1,26 @@
 DISTFILES += \
-	$$PWD/deploy_mac.command \
-	$$PWD/deploy_x11.sh \
-	$$PWD/deploy_win.bat
+	$$PWD/deploy.py
 
 !isEmpty(QTIFW_DEPLOY_SRC) {
 	isEmpty(QTIFW_DEPLOY_OUT): QTIFW_DEPLOY_OUT = $$OUT_PWD/deployed
 
-	#prepare common args
-	linux {
-		QTIFW_DEPLOY_ARGS += $$shell_quote($$[QT_INSTALL_PLUGINS])
-		QTIFW_DEPLOY_ARGS += $$shell_quote($$[QT_INSTALL_TRANSLATIONS])
-	}
+	linux: QTIFW_DEPLOY_ARGS = linux
+	else:win32:CONFIG(release, debug|release): QTIFW_DEPLOY_ARGS = win_release
+	else:win32:CONFIG(debug, debug|release): QTIFW_DEPLOY_ARGS = win_debug
+	else:mac: QTIFW_DEPLOY_ARGS = mac
+	else: QTIFW_DEPLOY_ARGS = unknown
 
+	QTIFW_DEPLOY_ARGS += $$shell_quote($$[QT_INSTALL_BINS])
+	QTIFW_DEPLOY_ARGS += $$shell_quote($$[QT_INSTALL_PLUGINS])
+	QTIFW_DEPLOY_ARGS += $$shell_quote($$[QT_INSTALL_TRANSLATIONS])
 	QTIFW_DEPLOY_ARGS += $$shell_quote($$QTIFW_DEPLOY_SRC)
 	QTIFW_DEPLOY_ARGS += $$shell_quote($$QTIFW_DEPLOY_OUT)
 	!isEmpty(QTIFW_DEPLOY_TSPRO): QTIFW_DEPLOY_ARGS += $$shell_quote($$QTIFW_DEPLOY_TSPRO)
 
 	qtifw_deploy.target = deploy
-	linux: qtifw_deploy.commands = $$shell_quote($$shell_path($$PWD/deploy_x11.sh)) $$QTIFW_DEPLOY_ARGS
-	else:win32: qtifw_deploy.commands = $$shell_quote($$shell_path($$PWD/deploy_win.bat)) $$QTIFW_DEPLOY_ARGS
-	else:mac: qtifw_deploy.commands = $$shell_quote($$shell_path($$PWD/deploy_mac.command)) $$QTIFW_DEPLOY_ARGS
+	linux: qtifw_deploy.commands = $$shell_quote($$shell_path($$PWD/deploy.py)) $$QTIFW_DEPLOY_ARGS
+	else:win32: qtifw_deploy.commands = python $$shell_quote($$shell_path($$PWD/deploy.py)) $$QTIFW_DEPLOY_ARGS
+	else:mac: qtifw_deploy.commands = /usr/local/bin/python3 $$shell_quote($$shell_path($$PWD/deploy.py)) $$QTIFW_DEPLOY_ARGS
 
 	QMAKE_EXTRA_TARGETS += qtifw_deploy
 }
---
+++ deploy/deploy.py
@@ -0,0 +1,129 @@
+#!/usr/bin/env python3
+
+import sys
+import os
+import shutil
+import glob
+import subprocess
+import re
+from distutils.dir_util import copy_tree
+from enum import Enum
+
+# constants
+platform = sys.argv[1]
+bindir = sys.argv[2]
+plugindir = sys.argv[3]
+translationdir = sys.argv[4]
+depsrc = sys.argv[5]
+outdir = sys.argv[6]
+profiles = ""
+if len(sys.argv) > 7:
+	profiles = sys.argv[7]
+
+binname = os.path.join(outdir, os.path.basename(depsrc))
+
+def run_deptool():
+	preparams = []
+	postparams = []
+	postcmds = []
+	if platform == "linux":
+		preparams = [os.path.join(bindir, "linuxdeployqt")]
+		postcmds = [
+			["rm", os.path.join(outdir, "AppRun")],
+			["cp", "-rPn", os.path.join(plugindir, "platforminputcontexts"), os.path.join(outdir, "plugins/")],
+			["cp", "-rPn", os.path.join(plugindir, "platformthemes"), os.path.join(outdir, "plugins/")],
+			["cp", "-rPn", os.path.join(plugindir, "xcbglintegrations"), os.path.join(outdir, "plugins/")]
+		]
+	elif platform == "win_debug":
+		preparams = [os.path.join(bindir, "windeployqt.exe"), "-debug", "-no-translations"]
+		postcmds = [
+			["del", os.path.join(outdir, "vcredist_x86.exe")],
+			["del", os.path.join(outdir, "vcredist_x64.exe")]
+		]
+	elif platform == "win_release":
+		preparams = [os.path.join(bindir, "windeployqt.exe"), "-release", "-no-translations"]
+		postcmds = [
+			["del", os.path.join(outdir, "vcredist_x86.exe")],
+			["del", os.path.join(outdir, "vcredist_x64.exe")]
+		]
+	elif platform == "mac":
+		preparams = [os.path.join(bindir, "macdeployqt")]
+		postparams = ["-appstore-compliant"]
+	else:
+		raise Exception("Unknown platform type: " + platform)
+
+	subprocess.run(preparams + [binname] + postparams)
+	for cmd in postcmds:
+		subprocess.run(cmd)
+
+def cp_translations():
+	transdir = ""
+	if platform == "mac":
+		transdir = os.path.join(binname , "Contents", "Resources", "translations")
+	else:
+		transdir = os.path.join(outdir, "translations")
+	os.makedirs(transdir, exist_ok=True)
+
+	trpatterns = [ #TODO complete list
+		"qt_??.qm",
+		"qt_??_??.qm",
+		"qtbase_*.qm",
+		"qtconnectivity_*.qm",
+		"qtdeclarative_*.qm",
+		"qtlocation_*.qm",
+		"qtmultimedia_*.qm",
+		"qtquick1_*.qm",
+		"qtquickcontrols_*.qm",
+		"qtscript_*.qm",
+		"qtserialport_*.qm",
+		"qtwebengine_*.qm",
+		"qtwebsockets_*.qm",
+		"qtxmlpatterns_*.qm"
+	]
+
+	for pattern in trpatterns:
+		for file in glob.glob(os.path.join(translationdir, pattern)):
+			dfile = os.path.join(transdir, os.path.basename(file))
+			shutil.copy2(file, dfile)
+
+	data = profiles.split(" ")
+	for prof in data:
+		subprocess.run([
+			os.path.join(bindir, "lrelease"),
+			"-compress",
+			"-nounfinished",
+			prof
+		])
+		for root, dirs, files in os.walk(os.path.dirname(prof)):
+			for f in files:
+				if os.path.splitext(f)[1] == ".qm":
+					dfile = os.path.join(transdir, f)
+					ofile = os.path.join(root, f)
+					shutil.copy2(ofile, dfile)
+
+def patch_qtconf(translationsPresent):
+	if platform == "linux":
+		return
+	elif platform == "win_debug" or platform == "win_release":
+		file = open(os.path.join(outdir, "qt.conf"), "w")
+		file.write("[Paths]\nPrefix=.")
+		file.close()
+	elif platform == "mac":
+		file = open(os.path.join(binname , "Contents", "Resources", "qt.conf"), "a")
+		file.write("Translations=Resources/translations\n")
+		file.close()
+	else:
+		raise Exception("Unknown platform type: " + platform)
+
+# prepare & copy files
+shutil.rmtree(outdir, ignore_errors=True)
+os.makedirs(outdir, exist_ok=True)
+shutil.copy2(depsrc, binname)
+
+# run the deployment tools
+run_deptool()
+if profiles != "":
+	cp_translations()
+	patch_qtconf(True)
+else:
+	patch_qtconf(False)
